9cd493d813390b5390d31196b9138e9779c61bf8
atsu85 <atsu85@gmail.com>
2016-06-23 00:19:15 +0300

    `typings install dt~undertaker --save-dev --global`

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -6,5 +6,6 @@
 /// <reference path="globals/jasmine/index.d.ts" />
 /// <reference path="globals/node/index.d.ts" />
 /// <reference path="globals/selenium-webdriver/index.d.ts" />
+/// <reference path="globals/undertaker/index.d.ts" />
 /// <reference path="globals/vinyl-fs/index.d.ts" />
 /// <reference path="modules/gulp/index.d.ts" />
